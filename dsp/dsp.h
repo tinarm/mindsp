07bcf3ced78dfdb8b8214a02a0b29bfbc5c734f6
Martin Navne <martin.navne@telavox.se>
2022-10-23 15:36:39 +0200

    Add basic stat functions
      * Mean
      * Standard deviation

--- dsp/dsp.h
+++ dsp/dsp.h
@@ -1,7 +1,26 @@
-#ifndef MAIN_HEADER_H
-#define MAIN_HEADER_H
+#ifndef LIB_DSP_HEADER_H
+#define LIB_DSP_HEADER_H
 
-/* Content of header file */
+#include <stddef.h>
+
+/**
+ * @brief Calculate the arithmetic mean of a signal
+ *
+ * @param signal Signal array
+ * @param len Length of the array
+ * @return Mean value of the signal
+ */
+float dsp_stat_get_mean(const float signal[], size_t len);
+
+/**
+ * @brief Calculate the standard deviation of a signal
+ *
+ * @param[in] signal Signal array
+ * @param len Length of the array
+ * @param mean Mean value of the signal
+ * @return Standard deviation of the signal
+ */
+float dsp_stat_get_stddev(const float signal[], size_t len, float mean);
+
 
 #endif
-
--- dsp/stat.c
+++ dsp/stat.c
@@ -1,8 +1,31 @@
+#include <math.h>
 #include <stdio.h>
+#include <stdint.h>
 
-int main(void)
+#include "dsp.h"
+
+
+float dsp_stat_get_mean(const float signal[], size_t len)
 {
-    printf("Hello, World!\n");
-    return 0;
+    float mean = 0.0f;
+
+    for (size_t i = 0; i < len; i++) {
+        mean += signal[i];
+    }
+
+    return mean / len;
 }
 
+
+float dsp_stat_get_stddev(const float signal[], size_t len, float mean)
+{
+    float variance = 0.0f;
+
+    for (size_t i = 0; i < len; i++) {
+        variance = variance + pow((signal[i] - mean), 2);
+    }
+
+    variance = variance / (len - 1);
+
+    return sqrtf(variance);
+}
