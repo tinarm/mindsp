cee12fedb029ec30b3adbd53f553cca27889e121
Martin Navne <martin.navne@acconeer.com>
2023-01-25 09:30:04 +0100

    First "test" code

--- Makefile
+++ Makefile
@@ -4,6 +4,8 @@ obj = $(src:.c=.o)
 test_src = $(wildcard test/*.c)
 test_obj = $(test_src:.c=.o)
 
+CC = gcc
+CFLAGS = -I./mindsp
 LDFLAGS = -lm
 
 .PHONY: all
@@ -12,10 +14,9 @@ all: clean libmindsp mindsptest
 libmindsp: $(obj)
 	ar -rcs $@.ar $^
 
-mindsptest: $(test_obj)
+mindsptest: $(test_obj) libmindsp.ar
 	$(CC) -Wall -Wextra -o $@ $^ $(LDFLAGS)
 
 .PHONY: clean
 clean:
 	rm -f $(obj) $(test_obj) libmindsp.ar mindsptest
-
--- test/main.c
+++ test/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
+#include "mindsp.h"
+
+#define ALEN(a) (sizeof(a) / sizeof((a)[0]))
+
 int main(void)
 {
     printf("Hello, Test!\n");
+
+    const float signal[] = {10.0f, 12.0f, 14.0f, 16.0f, 18.0f, 20.0f};
+
+    float mean = mindsp_stat_get_mean(signal, ALEN(signal));
+    
+    printf("Signal mean: %.2f\n", mean);
+    printf("Signal stddev: %.2f\n", mindsp_stat_get_stddev(signal, ALEN(signal), mean));
+
     return 0;
 }
 
